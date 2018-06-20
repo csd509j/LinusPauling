e38f05706764c5a8d6b4dd65b82bc543f91cfcce
Josh Armentano <josh@abidewebdesign.com>
2018-06-20 08:53:45 -0700

    Initial commit

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+ * Theme update checker
+ *
+ * @since CSD Schools 1.1
+ */
+require WP_CONTENT_DIR . '/plugins/plugin-update-checker-master/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/csd509j/LinusPauling',
+	__FILE__,
+	'LinusPauling'
+);
+
+$myUpdateChecker->setBranch('master'); 
+
+/*
+ * Setup style sheets
+ *
+ * @since CSD Schools 1.0
+ */
+function lpms_theme_enqueue_styles() {
+    
+	$parent_style = 'csdschools';
+	
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'lpms-style',
+	    get_stylesheet_directory_uri() . '/style.css',
+	    array( $parent_style ),
+	    wp_get_theme()->get('Version')
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'lpms_theme_enqueue_styles' );
